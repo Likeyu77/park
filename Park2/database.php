990a40f0e26e9778e0e9dbf580da1eddb28ff883
likeyu77 <likeyu1210@gmail.com>
2022-10-21 09:24:11 +1000

    auto create table

--- Park2/database.php
+++ Park2/database.php
@@ -9,10 +9,15 @@ $password = "php";
 
 $mysqli = new mysqli($host, $username, $password, $dbname);
 
-
-
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
 } 
 
+$sql = "CREATE TABLE IF NOT EXISTS `park_db`.`user` (`id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(128) NOT NULL , `email` VARCHAR(255) NOT NULL , `password_hash` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), UNIQUE (`email`)) ENGINE = InnoDB;";
+
+if ($conn->query($sql) == TRUE){
+    echo"";
+}else{
+    echo"error creating table: " . $conn->error;
+}
 return $mysqli;
